3f46436575044ef2a0b5b60808aeeac0f9eeb23f
Joe Wood <joewoodconsulting@outlook.com>
2017-03-08 07:16:04 -0500

    Filter out broken links

--- src/edge-flow-dag.tsx
+++ src/edge-flow-dag.tsx
@@ -3,10 +3,10 @@
  * */
 
 import * as React from "react";
-import { groupBy,  flatten, keyBy } from "lodash";
+import { groupBy, flatten, keyBy } from "lodash";
 // import Color = require("color");
 
-import { EdgeFlow, Edge, Node,  IBaseProps } from "./edge-flow"
+import { EdgeFlow, Edge, Node, IBaseProps } from "./edge-flow"
 import { Edge as EdgeDag, IEdgeDagProps } from "./edge-flow-dag/dag-edge";
 import { Node as NodeDag, INodeDagProps } from "./edge-flow-dag/dag-node";
 
@@ -39,23 +39,23 @@ export class EdgeFlowDag extends React.PureComponent<IProps, IState> {
 
     constructor(p: IProps) {
         super(p);
-        this.state = { nodes: this.getStateFromProps(p,true) };
+        this.state = { nodes: this.getStateFromProps(p, true) };
     }
 
-    private getStateFromProps(newProps: IProps,force=false): IPosNode[] {
+    private getStateFromProps(newProps: IProps, force = false): IPosNode[] {
         // for new props, we only want to calc the dag if the structure has changed
         type TrackChangesType = { width: number, height: number, links: { linkTo: string }[] };
         const newState = mapChild<INodeDagProps, TrackChangesType>(newProps.children,
             props => ({
                 width: props.width,
                 height: props.height,
-                links: mapChild<IEdgeDagProps, { linkTo: string }>(props.children, ({linkTo}) => ({ linkTo }))
+                links: mapChild<IEdgeDagProps, { linkTo: string }>(props.children, ({ linkTo }) => ({ linkTo }))
             }));
         const oldState = mapChild<INodeDagProps, TrackChangesType>(this.props.children,
             props => ({
                 width: props.width,
                 height: props.height,
-                links: mapChild<IEdgeDagProps, { linkTo: string }>(props.children, ({linkTo}) => ({ linkTo }))
+                links: mapChild<IEdgeDagProps, { linkTo: string }>(props.children, ({ linkTo }) => ({ linkTo }))
             }));
         if (force || !equals(newState, oldState)) {
             const graph = getGraphFromNodes(newProps.children as any);
@@ -75,10 +75,10 @@ export class EdgeFlowDag extends React.PureComponent<IProps, IState> {
     public render() {
         const state = this.state;
         const posNodes = keyBy(state.nodes, n => n.id);
-        const {children, ...props} = this.props;
+        const { children, ...props } = this.props;
         const nodes = getChildrenProps<INodeDagProps>(children) || [];
         console.log("Rendering DAG " + nodes.length);
-        // const nodeDict = keyBy(nodes, n => n.id);
+        const nodeDict = keyBy(nodes, n => n.id);
         const posEdges = flatten(state.nodes.map(n => n.edges.map(e => ({ ...e, id: n.id }))));
         const edgeDict = keyBy(posEdges, e => e.id + "-" + e.linkTo)
         type EdgeAndNodeType = IEdgeDagProps & { fromForceNode: string };
@@ -93,16 +93,18 @@ export class EdgeFlowDag extends React.PureComponent<IProps, IState> {
             nodes
                 .map(node => (
                     <Node key={node.id} center={posNodes[node.id]} {...node} >
-                        {groupedEdges[node.id] && groupedEdges[node.id].map(edge => {
-                            const ee = edgeDict[node.id + "-" + edge.linkTo];
-                            return <Edge key={edge.fromForceNode + "-" + edge.linkTo}
-                                {...edge}
-                                p0={ee.p0}
-                                p1={ee.p1}
-                                p2={ee.p2}
-                                p3={ee.p3}
-                            />;
-                        })}
+                        {groupedEdges[node.id] && groupedEdges[node.id]
+                            .filter(edge => nodeDict[edge.linkTo])
+                            .map(edge => {
+                                const ee = edgeDict[node.id + "-" + edge.linkTo];
+                                return <Edge key={edge.fromForceNode + "-" + edge.linkTo}
+                                    {...edge}
+                                    p0={ee.p0}
+                                    p1={ee.p1}
+                                    p2={ee.p2}
+                                    p3={ee.p3}
+                                />;
+                            })}
                     </Node>))
         }</EdgeFlow>
         );
